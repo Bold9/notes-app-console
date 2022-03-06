dcee7845a677762dd7e69c0d1a59b741adbcf92d
Bold9 <78432233+Bold9@users.noreply.github.com>
2022-03-06 17:37:50 +0000

    Skeleton menu for Notes written

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,27 +1,27 @@
+import utils.ScannerInput
 import java.lang.System.exit
-import java.util.*
+
+
+
 
 fun main(args: Array<String>) {
     runMenu()
 }
 
-val scanner = Scanner(System.`in`)
-
 fun mainMenu() : Int {
-    print("""
-         < ----------------------------------
-         < |        NOTE KEEPER APP         |
-         < ----------------------------------
-         < | NOTE MENU                      |
-         < |   1) Add a note                |
-         < |   2) List all notes            |
-         < |   3) Update a note             |
-         < |   4) Delete a note             |
-         < ----------------------------------
-         < |   0) Exit                      |
-         < ----------------------------------
-         < ==>> """.trimMargin(">"))
-    return scanner.nextInt()
+    return ScannerInput.readNextInt(""" 
+         > ----------------------------------
+         > |        NOTE KEEPER APP         |
+         > ----------------------------------
+         > | NOTE MENU                      |
+         > |   1) Add a note                |
+         > |   2) List all notes            |
+         > |   3) Update a note             |
+         > |   4) Delete a note             |
+         > ----------------------------------
+         > |   0) Exit                      |
+         > ----------------------------------
+         > ==>> """.trimMargin(">"))
 }
 
 fun runMenu() {
